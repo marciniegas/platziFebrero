dc217dc7d15bdef2f99019d131be9d9835e5298a
marciniegas <arc.miguelangel@gmail.com>
2022-02-14 11:56:40 -0500

    6 ejercicios codewars

---
+++ codewars/likes.js
@@ -0,0 +1,25 @@
+function likes(names) {
+    var cantidad=names.length
+  switch (names.length) {
+                case 0:
+                    return "no one likes this";
+                    break;
+                case 1:
+                    return`${names[0]} likes this`;
+                    break;
+    
+                case 2:
+                    return `${names[0]} and ${names[1]} like this`;
+                    break;
+            
+                case 3:
+                    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+                    break; 
+            
+                default:    
+                    return `${names[0]}, ${names[1]} and ${cantidad - 2} others like this`;
+                    break; 
+            }
+  // TODO
+}
+/// comunidad
